f62a1aec2fdc9cb4d931a6c04d5561fa717f582f
yunong <yunong.pan@rwth-aachen.de>
2021-06-23 04:17:41 +0200

    inrange.py update

--- inrange.py
+++ inrange.py
@@ -8,25 +8,24 @@ cap = cv2.VideoCapture('VID_20210507_091819.mp4')
 while(1):
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_red = np.array([0,100,0])
-    upper_red = np.array([10,255,255])
+    lower_red = np.array([0, 100, 0])
+    upper_red = np.array([10, 255, 255])
     mask = cv2.inRange(hsv, lower_red, upper_red)
-    img_Guassian_mask = cv2.GaussianBlur(mask,(13,13),0)
+    img_Gaussian_mask = cv2.GaussianBlur(mask, (13, 13), 0)
 
-    res = cv2.bitwise_and(frame,frame, mask= mask)
+    res = cv2.bitwise_and(frame, frame, mask = mask)
 
     res_rgb = cv2.cvtColor(res, cv2.COLOR_BGR2RGB)
 
-    img_Guassian = cv2.GaussianBlur(res_rgb,(5,5),0)
+    gray_img = cv2.cvtColor(res_rgb, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray_img, 100, 255, cv2.THRESH_BINARY)
 
-    gray_img = cv2.cvtColor(img_Guassian, cv2.COLOR_BGR2GRAY)
-    ret,thresh2 = cv2.threshold(gray_img,70,255,cv2.THRESH_BINARY)
+    res_2 = cv2.bitwise_and(frame,frame, mask = thresh)
+    res_2_Gaussian = cv2.GaussianBlur(res_2, (5, 5), 0)
 
-    res_2 = cv2.bitwise_and(frame,frame, mask= thresh2)
-
-    cv2.namedWindow('image',cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('image', 1000,1000)
-    cv2.imshow('image',res_2)
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('image', 1000, 1000)
+    cv2.imshow('image', res_2_Gaussian)
     
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
--- inrange_image.py
+++ inrange_image.py
@@ -5,22 +5,23 @@ import matplotlib.pyplot as plt
 
 frame = cv2.imread("./DSC00088.JPG")
 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-lower_red = np.array([1,100,0])
-upper_red = np.array([20,255,255])
+lower_red = np.array([1, 100, 0])
+upper_red = np.array([20, 255, 255])
 mask = cv2.inRange(hsv, lower_red, upper_red)
 
-res = cv2.bitwise_and(frame,frame, mask = mask)
+res = cv2.bitwise_and(frame, frame, mask = mask)
 
 res_rgb = cv2.cvtColor(res, cv2.COLOR_BGR2RGB)
 
-img_Guassian = cv2.GaussianBlur(res_rgb,(5,5),0)
+res_rgb_Gaussian = cv2.GaussianBlur(res_rgb, (5, 5), 0)
 
 
-gray_img = cv2.cvtColor(img_Guassian, cv2.COLOR_BGR2GRAY)
-ret,thresh2 = cv2.threshold(gray_img,30,255,cv2.THRESH_BINARY)
+gray_img = cv2.cvtColor(res_rgb_Gaussian, cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(gray_img, 30, 255, cv2.THRESH_BINARY)
 
-res_2 = cv2.bitwise_and(frame,frame, mask= thresh2)
-res_2_rgb = cv2.cvtColor(res_2, cv2.COLOR_BGR2RGB)
+res_2 = cv2.bitwise_and(frame,frame, mask= thresh)
+res_2_Gaussian = cv2.GaussianBlur(res_2, (5, 5), 0)
+res_2_rgb = cv2.cvtColor(res_2_Gaussian, cv2.COLOR_BGR2RGB)
 
 plt.imshow(res_2_rgb)
 plt.show()
